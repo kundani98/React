3ed6ebe22a352a890455ebe3c57bd5cb90856870
kundani98 <kcnemugumoni@gmail.com>
2023-06-20 09:21:53 +0200

    shopping

--- Projects/shopping_cart/shopping_cart/src/App.js
+++ Projects/shopping_cart/shopping_cart/src/App.js
@@ -2,15 +2,45 @@ import Header from './Components/Header'
 import Main from './Components/Main';
 import Basket from './Components/Basket';
 import data from './data';
-
+import { useState } from 'react';
 function App() {
   const {products} = data
+  const [cartItems,setCartItems] = useState([]); 
+    const onAdd = (product) =>{ 
+    const exist = cartItems.find(x=>x.id===product.id); 
+    if(exist){ 
+      setCartItems(cartItems.map(x=>x.id===product.id ?{...exist,qty:exist.qty+1}:x
+        )
+      )
+    }
+    else{ 
+      setCartItems([...cartItems,{ ...product,qty: 1}]);
+
+    }
+  }
+  const onRemove = (product)=> { 
+      const exist = cartItems.find((x) => x.id === product.id); 
+      if(exist.qty ===1){ 
+        setCartItems(cartItems.filter((x) => x.id !== product.id));
+
+      }else{ 
+      setCartItems(
+        cartItems.map((x)=> 
+          x.id == product.id ? {...exist, qty: exist.qty - 1} : x
+        ) 
+        ); 
+      }
+  };
   return (
     <div className="App">
-      <Header/> 
-     <div className="row">
-      <Main>products={products}</Main> 
-      <Basket/>
+     <Header countCartItems ={cartItems.length}></Header> 
+      <div className="row">
+      <Main onAdd={onAdd}  products={products}></Main> 
+      <Basket onAdd={onAdd}
+              onRemove={onRemove} 
+              cartItems={cartItems}>
+              
+      </Basket>
       </div>
     </div>
   );
--- Projects/shopping_cart/shopping_cart/src/Components/Header.js
+++ Projects/shopping_cart/shopping_cart/src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Header(props) {
-
+    const {countCartItems} = props
     return (
         <header class="row block center ">
             <div> 
@@ -10,7 +10,15 @@ function Header(props) {
                 </a>
 
             </div>
-            <a href="#/cart">Cart</a><a href="#signin">Sign In</a>
+            <a href="#/cart">
+                
+                Cart{' '}
+                {countCartItems ?( 
+                    <button className="badge">{countCartItems}</button>
+                 ) :
+                    ''
+                }
+                </a><a href="#signin">Sign In</a>
         </header>
         
         
